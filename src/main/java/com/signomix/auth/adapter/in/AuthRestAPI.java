f2bd8978730ce6676c65ac432837459f27c1b635
Grzegorz Skorupa <g.skorupa@gmail.com>
2023-12-01 01:20:33 +0100

    wip

--- src/main/java/com/signomix/auth/adapter/in/AuthRestAPI.java
+++ src/main/java/com/signomix/auth/adapter/in/AuthRestAPI.java
@@ -14,7 +14,6 @@ import jakarta.ws.rs.HeaderParam;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Response;
 
 @Path("/api/auth/v2")
